e4f679ed6496226ed922b7c4ded4676b6783e06e
madforre <33324825+madforre@users.noreply.github.com>
2019-01-08 16:24:05 +0900

    Blade Layout Files

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+
+{{-- 경로가 views / layouts / app 라면
+@extends('layouts.app')
+@extends('layouts/app') --}}
+
+@extends('layout')
+
+@section('title', 'About Us')
+
+@section('content')
+    
+<h1>About Us</h1>
+
+<p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt quo sint omnis et corporis deserunt culpa a quos dolores incidunt maiores, ducimus dicta, maxime dolor obcaecati facilis quod ut ab modi possimus minus, molestias natus? Numquam obcaecati facere voluptates sint. Porro quaerat natus dicta nam eos eveniet! Eum, voluptatem unde!
+</p>
+
+@endsection
